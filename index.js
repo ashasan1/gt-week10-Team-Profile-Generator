964c699fd32a91ce162ed464831bf3ae1a31b4cc
Asha Sanford <sanford.asha97@yahoo.com>
2021-04-22 00:32:15 -0400

    testing makeHTML function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 
+let fullTeam = [];
+
 const managerQuestions = [
     {
         type: 'input',
@@ -17,16 +19,50 @@ const managerQuestions = [
 
 // inquirer.prompt(teamQuestions).then((response))
 
+// function makeManager() {
+//     inquirer.prompt(managerQuestions).then(function (response) {
+//         let teamManager = `${response.manager}`;
+//         fullTeam.push(teamManager);
+//     }
+//     )};
+
 function makeManager() {
-    inquirer.prompt(managerQuestions).then((response) =>
-    makeCard("manager", 
-    `Name of Team Member: ${response.manager} 
-    Email of Team Member: ${response.managerEmail}`)
-    )
+    inquirer.prompt(managerQuestions).then( function (response) {
+        const teamManager = `${response.manager}`;
+        fullTeam.push(teamManager);
+    })
 }
 
-function makeCard(member,input) {
-    fs.writeFile(`${member}.html`, input, (err) =>
+
+
+// function makeCard(member,input) {
+//     fs.writeFile(`${member}.html`, input, (err) =>
+//     err ? console.error(err) : console.log(`Success!`)
+//     )
+// }
+
+function makeTeamHTML(input) {
+    fs.writeFile(`team.html`), input, (err) =>
     err ? console.error(err) : console.log(`Success!`)
-    )
 }
+
+makeManager();
+makeTeamHTML(
+    `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    
+    <div>
+    <h1>Team Member Position: </h1>
+    </div>
+        
+    </body>
+    </html>`
+    );
